0d22f9cd65f0cfe1ac8676511b5df46820bff9d8
abilashr <abilaashsai@gmail.com>
2017-06-09 23:48:21 +0530

    Sai Kailash App Initiated with Thought for the day screen

--- SaiKailash/FirstViewController.swift
+++ SaiKailash/FirstViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class FirstViewController: UIViewController {
-
+    var ref: DatabaseReference!
+    
+    @IBOutlet weak var thoughtDetail: UITextView!
+    @IBOutlet weak var thoughtTitle: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
+        ref.child("thought").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            var value = snapshot.value as? NSDictionary
+            var title = value?["title"] as! String
+            var detail = value?["data"] as! String
+            self.thoughtTitle.text = title;
+            self.thoughtDetail.text = detail;
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
