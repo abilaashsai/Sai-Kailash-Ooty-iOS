3e1f48cdbf18f61dd8c8dad707161c1bc40dba8c
abilashr <abilaashsai@gmail.com>
2017-06-11 00:52:49 +0530

    Populate events in Upcoming Events

--- SaiKailash/SecondViewController.swift
+++ SaiKailash/SecondViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
 
+    var ref: DatabaseReference!
+    
+
+    var list = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ref = Database.database().reference()
+
+        
+        ref.child("events/upcoming").observeSingleEvent(of: .value, with: { (snapshot) in
+            for year in snapshot.children{
+                for month in (year as! DataSnapshot).children{
+                    for day in (month as! DataSnapshot).children{
+                        var dayObject = (day as! DataSnapshot).value as? NSDictionary
+                        var date = dayObject?["date"] as! String
+                        var message = dayObject?["message"] as! String
+                        self.list.append(date+"   "+message)
+                    }
+                }
+                self.tableView.reloadData()
+                
+            }
+            
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
     }
-
-
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return(list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return(cell)
+    }
+    
 }
 
